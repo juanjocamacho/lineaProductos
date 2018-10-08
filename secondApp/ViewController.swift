76fd556f4cee235bee9c5c72d605e69aecd3ee8d
Juanjo <klou@Mac-de-Juanjo.local>
2018-10-08 15:05:52 +0200

    Initial Commit

---
+++ secondApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  secondApp
+//
+//  Created by Juanjo on 8/10/18.
+//  Copyright © 2018 Juan José Camacho Hidalgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
